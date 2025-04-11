e0cf3327bd5f1404d262970608297c40a5c6a095
Shivam prasad <shivam13juna@gmail.com>
2025-04-11 22:17:11 +0530

    adding codes for travelling salesman.

---
+++ travelling_salesman.py
@@ -0,0 +1,29 @@
+def solve_tsp(distance_matrix):
+	"""
+	Dummy function for solving the Traveling Salesman Problem.
+	This function should eventually implement an efficient TSP algorithm.
+	
+	Parameters:
+		distance_matrix (list of list of float): A matrix where each element [i][j]
+			represents the distance from city i to city j.
+	
+	Returns:
+		list of int: A dummy route that visits cities in sequential order.
+	"""
+	num_cities = len(distance_matrix)
+	if num_cities == 0:
+		return []
+	
+	# Dummy solution: just return a route visiting cities in sequential order.
+	return list(range(num_cities))
+
+
+if __name__ == "__main__":
+	# Example usage with a sample distance matrix
+	dummy_distance_matrix = [
+		[0, 10, 15],
+		[10, 0, 20],
+		[15, 20, 0]
+	]
+	route = solve_tsp(dummy_distance_matrix)
+	print("Dummy TSP route:", route)
